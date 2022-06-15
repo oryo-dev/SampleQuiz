bff28750de4a85b893b792c10f79d949cb90def6
oryo-dev <oryo.dev@gmail.com>
2022-06-16 08:37:39 +0900

    Add scoreView logic

--- SampleQuiz/SampleQuiz/ScoreViewController.swift
+++ SampleQuiz/SampleQuiz/ScoreViewController.swift
@@ -8,22 +8,18 @@
 import UIKit
 
 class ScoreViewController: UIViewController {
-
+    
+    @IBOutlet weak var scoreLabel: UILabel!
+    
+    var correctCount = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        scoreLabel.text = "\(correctCount)問正解！"
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func toTopButtonAction(_ sender: Any) {
+        self.presentingViewController?.presentingViewController?.dismiss(animated: true)
     }
-    */
-
 }
